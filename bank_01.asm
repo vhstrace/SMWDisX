407107bbb03f799a74efb5fe12b41e0e0a06c17e
VHS Trace <vhstrace@gmail.com>
2026-09-13 10:57:44 -0700

    Rename SpawnSkidSmoke from CODE_01804E
    Used for sliding shells and Monty Mole

--- bank_01.asm
+++ bank_01.asm
@@ -71,7 +71,7 @@ InvertAccum:
     INC A                                     ; /
     RTS
 
-CODE_01804E:
+SpawnSkidSmoke:
     LDA.W SpriteBlockedDirs,X                 ; \ Branch if in air
     BEQ Return018072                          ; /
     LDA.B TrueFrame
@@ -1504,7 +1504,7 @@ CODE_0189B4:
   + SEC
     SBC.B _0
     STA.B SpriteXSpeed,X
-    JSR CODE_01804E
+    JSR SpawnSkidSmoke
 CODE_0189E6:
     STZ.W SpriteMisc1570,X
     JSR CODE_018B43
@@ -1567,7 +1567,7 @@ CODE_018A29:
     SBC.B _0
     STA.W SpriteXSpeed,Y
     STA.B SpriteXSpeed,X
-    JSR CODE_01804E
+    JSR SpawnSkidSmoke
 CODE_018A62:
     STZ.W SpriteMisc1570,X
     JSR CODE_018B43
@@ -13460,7 +13460,7 @@ CODE_01E393:
     ROR A
     EOR.B SpriteXSpeed,X
     BPL +
-    JSR CODE_01804E
+    JSR SpawnSkidSmoke
     JSR SetAnimationFrame
   + RTS
 
